3e336cf12e8f3e438198bacfb8e29f4d70b09a8e
d-ori <jiographie@gmail.com>
2025-11-25 10:46:49 +0900

    feat: applying tanstack query to manage asynchronous todo data

--- src/pages/todo-list-page.tsx
+++ src/pages/todo-list-page.tsx
@@ -1,14 +1,17 @@
 import TodoEditor from "@/components/todo-list/todo-editor";
 import TodoItem from "@/components/todo-list/todo-item";
-import { useTodos } from "@/store/todos";
+import { useTodosData } from "@/hooks/queries/useTodosData";
 
 export default function TodoListPage() {
-  const todos = useTodos();
+  const { data: todos, isLoading, error } = useTodosData();
+
+  if (error) return <div>오류가 발생했습니다</div>;
+  if (isLoading) return <div>로딩중입니다</div>;
 
   return (
     <div className="flex flex-col gap-5 p-5">
       <TodoEditor />
-      {todos.map((todo) => (
+      {todos?.map((todo) => (
         <TodoItem key={todo.id} {...todo} />
       ))}
     </div>
